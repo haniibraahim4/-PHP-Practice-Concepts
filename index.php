18463f826305c66e84509cde22d3b68e197f6a14
imhanaibrahim2004-pixel <imhanaibrahim2004@gmail.com>
2026-09-19 23:34:16 +0300

    My PHP practice

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+// echo
+//     // "<h1> welcome  to Home Php me</h1>";
+//     // print"this is a simple PHP file"
+   
+//     // print "ca1233","7";
+//     $Fname="hani ibraahim";
+//     echo" my name is: $Fname";
+
+  $my_str="Welcome to Php Republic  ";
+  echo strlen($my_str)
+
+
+  $mystrt ="hani ibrahim Abouker";
+  echo str_word_count($mystrt)
+//     ?>
+
+  
+</body>
+</html>
